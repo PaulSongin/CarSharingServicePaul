343c2c3a4e42ac654458a7905d3cc4f83919bf34
Paul Songin <songinpavel2006@gmail.com>
2026-01-28 17:49:05 +0300

    Format annotations in Car.java for consistency

--- src/main/java/com/paul/carsharing/model/Car.java
+++ src/main/java/com/paul/carsharing/model/Car.java
@@ -5,17 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
-    @Entity                  // Говорит Спрингу: "Это таблица в БД"
-    @Table(name = "cars")    // Имя таблицы будет "cars"
-    @Data                    // Lombok: генерирует геттеры, сеттеры, toString
-    @NoArgsConstructor       // Lombok: пустой конструктор (нужен Hibernate)
+    @Entity                  
+    @Table(name = "cars")    
+    @Data                    
+    @NoArgsConstructor     
     public class Car {
 
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY) // ID будет расти сам (1, 2, 3...)
+        @GeneratedValue(strategy = GenerationType.IDENTITY) 
         private Long id;
 
-        @Column(nullable = false) // Не может быть пустым
+        @Column(nullable = false) 
         private String brand;
 
         @Column(nullable = false)
